8535766f14f62fef7448c62732c31177fbb0261b
Nedyalko Dyakov <me@ndyakov.eu>
2015-03-02 00:20:41 +0200

    Add comments for the middlewares.

--- middlewares.go
+++ middlewares.go
@@ -10,6 +10,8 @@ import (
 	"github.com/zenazn/goji/web"
 )
 
+// Response will set the Content-Type of the http response to:
+//     "application/json"
 func Response(h http.Handler) http.Handler {
 	fn := func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json")
@@ -17,6 +19,16 @@ func Response(h http.Handler) http.Handler {
 	return http.HandlerFunc(fn)
 }
 
+// Request will parse the request body to a whatever.Params structure and
+// then add this structure to the goji context map with the key "Params".
+// The error (or nil) of the decoding will be available in the context with
+// the key "GojisonDecodeError".
+//
+// The parsing of the body will happen only if the Content-Type of the request
+// is application/json.
+//
+// For more information about how to work with the whatever.Params type, please refer to:
+// http://godoc.org/github.com/ndyakov/whatever
 func Request(c *web.C, h http.Handler) http.Handler {
 	fn := func(w http.ResponseWriter, r *http.Request) {
 		contentTypeSlice := strings.Split(r.Header.Get("Content-Type"), ";")
