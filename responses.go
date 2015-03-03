53a8f96e8d1d3ccf6a0b0c5517b3fdf1129583a5
Nedyalko Dyakov <me@ndyakov.eu>
2015-03-03 14:02:03 +0200

    Add documentation about the Success and Error responses.

--- responses.go
+++ responses.go
@@ -5,17 +5,27 @@ import (
 	"net/http"
 )
 
+// Error recieves http.ResponseWriter, error interface and
+// response code. Sets the code to the ResponseWriter header
+// and then writes out the error as JSON.
+//
+// If the code parameter is 0, http.StatusBadRequest will be used.
 func Error(w http.ResponseWriter, err error, code int) {
 	if code == 0 {
-		code = 400
+		code = http.StatusBadRequest
 	}
 	w.WriteHeader(code)
 	json.NewEncoder(w).Encode(map[string]string{"error": err.Error()})
 }
 
+// Success recieves http.ResponseWriter, success message and a
+// response code. Sets the code to the ResponseWriter header
+// and then writes out the message as JSON.
+//
+// If the code parameter is 0, http.StatusOK will be used.
 func Success(w http.ResponseWriter, message string, code int) {
 	if code == 0 {
-		code = 200
+		code = http.StatusOK
 	}
 	w.WriteHeader(code)
 	json.NewEncoder(w).Encode(map[string]string{"success": message})
